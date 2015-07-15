9cb83df77e926129b01d7be65c74889a8f98f459
shuhei <kawashu0723@gmail.com>
2015-07-15 23:33:25 +0900

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Jump::Application.routes.draw do
   root :to => "jump#input"
   get "jump/input"
   get "jump/output"
-  get "jump/update"
+  get "jump" => "jump#update"
   get "jump/:url" => "jump#update", format: false, url: /.*/
 end
